fe49de106c4572d5f4f2e8333f71510400874982
joaovam <joao.vieira.1275044@sga.pucminas.br>
2022-11-09 23:24:43 -0300

    deploy

--- recognition.py
+++ recognition.py
@@ -24,6 +24,7 @@ def detect(data):
     img_array = tf.keras.utils.img_to_array(data)
     img_array = tf.expand_dims(img_array, 0)  # Create a batch
     img_array = preprocess_input(img_array)
+    img_array = img_array.reshape((96,96))
     mask, withoutMask = model.predict(img_array)[0]
 
     # determine the class label and color we will use to draw the bounding box and text
